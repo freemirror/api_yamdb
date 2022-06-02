b39b6813f2e51d0aa15143a6a4fff6aaccd1d690
Дмитрий Красилов <krasilov.d-post@yandex.ru>
2022-06-02 13:30:19 +0800

    Add permissions

---
+++ api_yamdb/api/permissions.py
@@ -0,0 +1,19 @@
+from rest_framework import permissions
+
+
+class SpecialPermission(permissions.BasePermission):
+    """Доступ для чтения всем пользователям,
+    доступ на создание для аутентифицированного пользователя,
+    доступ для изменения или удаления у автора, модератора и админа.
+    """
+
+    def has_permission(self, request, view):
+        return (
+            request.method in permissions.SAFE_METHODS
+            or request.user.is_authenticated
+        )
+
+    def has_object_permission(self, request, view, obj):
+        if request.method in permissions.SAFE_METHODS:
+            return True
+        return obj.author == request.user or request.user.is_staff
--- api_yamdb/api/views.py
+++ api_yamdb/api/views.py
@@ -1,13 +1,14 @@
 from django.shortcuts import get_object_or_404
 from rest_framework import viewsets
 
+from .permissions import SpecialPermission
 from .serializers import CommentSerializer, ReviewSerializer
 from reviews.models import Review, Title
 
 
 class CommentViewSet(viewsets.ModelViewSet):
     serializer_class = CommentSerializer
-    # permission_classes = (AuthorOrReadOnly,)
+    permission_classes = (SpecialPermission,)
 
     def get_queryset(self):
         review_id = self.kwargs.get("review_id")
@@ -24,6 +25,7 @@ class CommentViewSet(viewsets.ModelViewSet):
 
 class ReviewViewSet(viewsets.ModelViewSet):
     serializer_class = ReviewSerializer
+    permission_classes = (SpecialPermission,)
 
     def get_queryset(self):
         title_id = self.kwargs.get("title_id")
